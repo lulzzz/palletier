6b60d3e5748938878d419dd63b2e00cfe0fba31b
Alan Velasco <AlanVelasco.A@gmail.com>
2017-08-18 17:39:27 -0500

    Rework some name and weight logic on Box

--- palletier/box.py
+++ palletier/box.py
@@ -9,12 +9,15 @@ class Box:
     idx_gen = itertools.count(start=0, step=1)
 
     def __init__(self, dims, weight=0, idx=None, pos=Coords(0, 0, 0),
-                 orientation=Dims(0, 0, 0), name='N/A'):
+                 orientation=Dims(0, 0, 0), name=None):
         if idx is not None:
             self.idx = idx
         else:
             self.idx = next(Box.idx_gen)
-        self.name = name
+        if name is not None:
+            self.name = name
+        else:
+            self.name = 'NoName'
         self.is_packed = False
         self.dims = Dims(*dims)
         self.weight = weight
@@ -28,7 +31,11 @@ class Box:
         return self.idx == other.idx
 
     def __repr__(self):
-        repr_str = f'Box(idx={self.idx}, dims={self.dims}, weight={self.weight}'
+        repr_str = f'Box(idx={self.idx}, dims={self.dims}'
+        if self.weight != 0:
+            repr_str += f', weight={self.weight}'
+        if self.name != 'NoName':
+            repr_str += f'name={name}'
         if self.pos != Coords(0, 0, 0):
             repr_str += f', pos={self.pos}'
         if self.orientation != Dims(0, 0, 0):
@@ -37,7 +44,7 @@ class Box:
         return repr_str
 
     def __str__(self):
-        output = f'Box({self.dims.dim1}, {self.dims.dim2}, {self.dims.dim3}'
+        output = f'Box([{self.dims.dim1}, {self.dims.dim2}, {self.dims.dim3}]'
         if self.weight != 0:
             output += f', weight={self.weight}'
         output += ')'
