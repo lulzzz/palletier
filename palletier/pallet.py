ddff05857ede46af2b39b312b140df07358ddee7
Alan Velasco <AlanVelasco.A@gmail.com>
2017-08-18 17:39:45 -0500

    Update repr and str representations of Pallet

--- palletier/pallet.py
+++ palletier/pallet.py
@@ -8,14 +8,16 @@ Dims = namedtuple('Dims', ['dim1', 'dim2', 'dim3'])
 class Pallet:
     idx_gen = itertools.count(start=0, step=1)
 
-    def __init__(self, dims, idx=None, max_weight=20, name='N/A', ptype=1,
+    def __init__(self, dims, idx=None, max_weight=0, name=None,
                  orientation=Dims(0, 0, 0)):
         if idx is not None:
             self.idx = idx
         else:
             self.idx = next(Pallet.idx_gen)
-        self.name = name
-        self.type = ptype
+        if name is not None:
+            self.name = name
+        else:
+            self.name = 'NoName'
         self.dims = Dims(*dims)
         self.orientation = orientation
         self.weight = 0
@@ -25,6 +27,19 @@ class Pallet:
             self.vol *= dim
 
     def __repr__(self):
-        repr_str = 'Pallet(idx={0}, dims={1}, max_weight={2}, name={3}, type={4})'
-        return repr_str.format(self.idx, self.dims, self.max_weight, self.name,
-                               self.type)
+        repr_str = f'Pallet(idx={self.idx}, dims={self.dims}'
+        if self.max_weight != 0:
+            repr_str += f', max_weight={self.max_weight}'
+        if self.name != 'NoName':
+            repr_str += f', name={self.name}'
+        if self.orientation != Dims(0, 0, 0):
+            repr_str += ', orientation={self.orientation}'
+        repr_str += ')'
+        return repr_str
+
+    def __str__(self):
+        output = f'Pallet([{self.dims.x}, {self.dims.y}, {self.dims.z}]'
+        if self.max_weight != 0:
+            output += f', max_weight={self.max_weight}'
+        output += ')'
+        return output
